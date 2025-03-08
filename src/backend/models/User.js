234c827ea8a10be84f1ceec1d225af941cf088c8
Satyam1345 <Satyamkumar1345@gmail.com>
2025-03-08 21:17:18 +0530

    Models for User and Mahabharat done, each vedic scripture requires a separate model  and need to be initialized one by one.

---
+++ src/backend/models/User.js
@@ -0,0 +1,32 @@
+import mongoose from "mongoose" ; 
+
+const emailRegexSimple = /^\S+@\S+\.\S+$/ ;
+
+const UserSchema = new mongoose.Schema({
+    name : {
+        type : String , 
+        required : [true , "Please enter your Name..."]
+    },
+    email : {
+        type : String , 
+        required: true , 
+        unique : true ,
+        match : [emailRegexSimple , "Please use a valid email address"]
+    },
+    password : {
+        type : String ,
+        required : [true , "Please enter a valid password..."]
+    },
+    authProvider : {
+        type : String , 
+        enum : ["email" , "google"] , 
+        default : "email" 
+    },
+    bookmarks : [{
+        type : mongoose.Schema.Types.ObjectId , ref : "Shloka"
+    }]
+    } , {timestamps : true}
+
+);
+
+export default mongoose.models.User || mongoose.model("User" , UserSchema) ;
