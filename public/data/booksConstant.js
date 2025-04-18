66c5d03232e48f21a814ac22542a414c33ca6657
Satyam1345 <Satyamkumar1345@gmail.com>
2025-04-18 23:55:30 +0530

    bookConstants updated

--- public/data/booksConstant.js
+++ public/data/booksConstant.js
@@ -1,8 +1,52 @@
-const books = {
-  1: { name: "Ramayana", image: "ramayana.jpg", slug: "Ramayana" },
-  2: { name: "Mahabharata", image: "mahabharat.webp", slug: "Mahabharata" },
-  3: { name: "Bhagavad Gita", image: "bhagavad-gita.jpg", slug: "Bhagavad Gita" },
-  4: { name: "AtharvaVeda", image: "vedas2.jpg", slug: "AtharvaVeda" },
-  5: { name: "Rigveda", image: "vedas.avif", slug: "Rigveda" },
+// / public/data/booksConstant.js
+
+const booksData = {
+  1: {
+    name: "Ramayana",
+    image: "ramayana.jpg",
+    slug: "Ramayana",
+    books: [1],
+    chapters: {
+      1: [1],
+    },
+  },
+  2: {
+    name: "Mahabharata",
+    image: "mahabharat.webp",
+    slug: "Mahabharata",
+    books: [1, 2],
+    chapters: {
+      1: [1, 2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30],
+      2: [1, 2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30],
+    },
+  },
+  3: {
+    name: "Bhagavad Gita",
+    image: "bhagavad-gita.jpg",
+    slug: "Bhagavad Gita",
+    books: [1],
+    chapters: {
+      1: [1],
+    },
+  },
+  4: {
+    name: "AtharvaVeda",
+    image: "vedas2.jpg",
+    slug: "AtharvaVeda",
+    books: [1],
+    chapters: {
+      1: [1],
+    },
+  },
+  5: {
+    name: "Rigveda",
+    image: "vedas.avif",
+    slug: "Rigveda",
+    books: [1],
+    chapters: {
+      1: [1],
+    },
+  },
 };
-export default books;
+
+export default booksData;
